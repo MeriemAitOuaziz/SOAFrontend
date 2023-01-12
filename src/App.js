892e0de3650e9ed8a6d860d7863a13a2eb46b4df
ChaimaaNajim <nmchaimaa9@gmail.com>
2023-01-12 03:08:02 +0100

    added submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import MissionTable from './Components/MissionTable';
-import RemboursementTable from './Components/RemboursementTable'
-import { Button } from "@mui/material";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import MissionTable from "./Components/MissionTable";
+import RemboursementTable from "./Components/RemboursementTable";
+import { Button, TextField, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AddRemboursementModal from "./Components/AddRemboursementModal";
+import BasicModalDialog from "./Components/AddRemModal";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -37,51 +40,83 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
 export default function App() {
   const [value, setValue] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
           <Tab label="Missions" {...a11yProps(0)} />
           <Tab label="Remboursements" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <MissionTable/>
+        <MissionTable />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        
-        <Box display="flex" flexDirection="row" 
+        <Box
+          display="flex"
+          flexDirection="row"
           justifyContent="space-between"
           alignItems="center"
-          width="100%">
-        Remboursements
-        <Button
-              variant="contained"
-              sx={{ m: "10px", backgroundColor: "#70d8bd" }}
-            >
-              <Typography variant="h7">
-                Ajouter Remboursement
-              </Typography>
-            </Button>
+          width="100%"
+        >
+          <Typography variant="h4" fontWeight="bold" sx={{ mb: "5px" }}>
+            Remboursements
+          </Typography>
+          <TextField
+            margin="10px"
+            id="outlined-basic"
+            label="Chercher Remboursements par demandeur"
+            variant="outlined"
+            size="small"
+            style = {{width: 500}}
+          />
+          <IconButton aria-label="delete" size="small">
+            <SearchIcon fontSize="inherit" />
+          </IconButton>
 
+          <Button
+            variant="contained"
+            sx={{ m: "10px", backgroundColor: "#70d8bd" }}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <Typography variant="h7">Ajouter Remboursement</Typography>
+          </Button>
+         {/*} <AddRemboursementModal
+              id="ringtone-menu"
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              value="Dione"
+          />*/}
+          <BasicModalDialog
+          open={open}
+          />
+          
         </Box>
-
-        
 
         <RemboursementTable />
       </TabPanel>
-    
     </Box>
   );
 }
--- src/Components/RemboursementTable.jsx
+++ src/Components/RemboursementTable.jsx
@@ -1,52 +1,73 @@
-import { DataGrid } from '@mui/x-data-grid';
-import { Box, Typography, useTheme } from "@mui/material";
-import { colors } from 'debug/src/browser';
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Typography, useTheme, Button } from "@mui/material";
+import { colors } from "debug/src/browser";
 
 const rows = [
-    {id:1,ID:1,demandeur:"Ahmed",validPrice:500,ordre: 1, budgetAttrib:900,remboursementAttrib: 800, isValid: true},
-    {id:2,ID:1,demandeur:"Ahmed",validPrice:500,ordre: 1, budgetAttrib:900,remboursementAttrib: 800, isValid: false}
-  ];
-  
+  {
+    id: 1,
+    ID: 1,
+    demandeur: "Ahmed",
+    validPrice: 500,
+    ordre: 1,
+    budgetAttrib: 900,
+    remboursementAttrib: 800,
+    isValid: true,
+  },
+  {
+    id: 2,
+    ID: 1,
+    demandeur: "Ahmed",
+    validPrice: 500,
+    ordre: 1,
+    budgetAttrib: 900,
+    remboursementAttrib: 800,
+    isValid: false,
+  },
+];
 
-  const columns = [
-    { field: 'ID', headerName: 'Remboursement ID', width: 150 },
-    { field: 'demandeur', headerName: 'Demandeur', width: 150 },
-    { field: 'validPrice', headerName: 'Prix Valide', width: 150 },
-    { field: 'ordre', headerName: 'Ordre', width: 150 },
-    { field: 'budgetAttrib', headerName: 'Budget Attribué', width: 200 },
-    { field: 'remboursementAttrib', headerName: 'Remboursement Attribué', width: 200 },
-    {
-      field: "isValid",
-      headerName: "Validation",
-      renderCell: ({ row: { isValid } }) => {
-        return (
-          <Box
-            width="60%"
-            m="0 auto"
-            p="5px"
-            display="flex"
-            justifyContent="center"
-            backgroundColor={
-              isValid 
-                ? "#a3a3a3"
-                : "#70d8bd" 
-            }
-            borderRadius="4px"
-          >
-           
-            <Typography  sx={{ ml: "5px" }}>
-              {isValid} 
-            </Typography>
-          </Box>
-        );
-      },
+const columns = [
+  { field: "ID", headerName: "Remboursement ID", width: 150 },
+  { field: "demandeur", headerName: "Demandeur", width: 150 },
+  { field: "validPrice", headerName: "Prix Valide", width: 150 },
+  { field: "ordre", headerName: "Ordre", width: 150 },
+  { field: "budgetAttrib", headerName: "Budget Attribué", width: 200 },
+  {
+    field: "remboursementAttrib",
+    headerName: "Remboursement Attribué",
+    width: 200,
+  },
+  {
+    field: "isValid",
+    headerName: "Validation",
+    renderCell: ({ row: { isValid } }) => {
+      return (
+        <Box
+          width="60%"
+          m="0 auto"
+          p="5px"
+          display="flex"
+          justifyContent="center"
+          borderRadius="4px"
+        >
+          {isValid ? (
+            <Button variant="contained" color="success" disabled fullWidth>
+              Validé
+            </Button>
+          ) : (
+            <Button variant="contained" color="success" fullWidth>
+              "Valider"
+            </Button>
+          )}
+        </Box>
+      );
     },
-  ];
+  },
+];
 
-  export default function RemboursementTable() {
-    return (
-      <div style={{ height: 300, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} />
-      </div>
-    );
-  }
+export default function RemboursementTable() {
+  return (
+    <div style={{ height: 300, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
+}
